7219972c7abe91f29da0e94945fc8ef9e2e09ff0
Nikitos <nikita4651@yandex.ru>
2026-06-23 16:42:18 +0300

    Тест успешен

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
     testImplementation ("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation ("org.seleniumhq.selenium:selenium-java:4.29.0")
     testImplementation ("io.github.bonigarcia:webdrivermanager:5.9.3")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
+
 }
 
 tasks.test {
---
+++ src/test/java/ru/netology/servic/DebetCardTest.java
@@ -0,0 +1,56 @@
+package ru.netology.servic;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DebetCardTest {
+
+    private WebDriver driver;
+
+
+    @BeforeEach
+    void setup() {
+            driver = WebDriverManager.chromedriver().create();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+      options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+                driver = null;
+    }
+
+    @Test
+    void Test1() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Осепчук Никита");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79110126430");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+        WebElement result =  driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim());
+
+
+
+    }
+}
