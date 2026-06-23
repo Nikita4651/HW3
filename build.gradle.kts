8b43a5596c71e12a62f2346a6f97d05a55c08583
Nikitos <nikita4651@yandex.ru>
2026-06-23 11:54:14 +0300

    add build.gradle.kts and .github/workflows/gradle.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,33 @@
 plugins {
-    id("java")
+    id ("java")
 }
 
 group = "ru.netology"
 version = "1.0-SNAPSHOT"
+
+// используем 11 версию языка
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+// кодировка файлов (если используете русский язык в файлах)
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation ("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation ("org.seleniumhq.selenium:selenium-java:4.29.0")
+    testImplementation ("io.github.bonigarcia:webdrivermanager:5.9.3")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // свойство chromeoptions.prefs используется для задания настроек браузера в проектах на основе Selenide, выключаем менеджер паролей
+    systemProperty("chromeoptions.prefs", System.getProperty("chromeoptions.prefs", "profile.password_manager_leak_detection=false"))
 }
